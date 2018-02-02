d96f3790d6c45530f64e282b2811d38e87e98054
tmarshall <timothyjmarshall@gmail.com>
2018-02-01 17:49:46 -0800

    fix crawl logic, for first dir

--- sync-crawl/index.js
+++ sync-crawl/index.js
@@ -8,57 +8,61 @@ const validVerbs = ['all', 'get', 'post', 'put', 'patch', 'delete'];
 const startingDollarSign = /^\$/;
 const jsFileExt = /\.js$/;
 
-function syncCrawlRoutesDir(dirpath, uriPathTokens) {
-  if (arguments.length === 1) {
-    // at first call, only a directory path is given
-    uriPathTokens = [];
-  }
+function syncCrawlRoutesDir(rootpath) {
+  let firstCrawl = true;
 
-  const base = path.parse(dirpath).base;
+  function getRoutes(dirpath, uriPathTokens = []) {
+    const base = path.parse(dirpath).base;
 
-  // first directory is not added to the uri path, for the express routing
-  if (uriPathTokens.length) {
-    // adding to the tokens of the express route, based on the current directory being crawled
-    // a folder starting with a $ will be considered a req param
-    // (The : used in express does not work well in directory naming, and will mess up directory searching)
-    uriPathTokens.push(base.replace(startingDollarSign, ':'));
-  }
-
-  const list = fs.readdirSync(dirpath);
-  const routes = [];
-  const files = [];
-
-  sortInsensitive(list);
-
-  for (let i = 0; i < list.length; i++) {
-    const stat = fs.statSync(path.resolve(dirpath, list[i]));
-
-    if (stat.isFile() && jsFileExt.test(list[i])) {
-      files.push(list[i]);
-      continue;
+    // first directory is not added to the uri path, for the express routing
+    if (firstCrawl === false) {
+      // adding to the tokens of the express route, based on the current directory being crawled
+      // a folder starting with a $ will be considered a req param
+      // (The : used in express does not work well in directory naming, and will mess up directory searching)
+      uriPathTokens.push(base.replace(startingDollarSign, ':'));
+    } else {
+      firstCrawl = false;
     }
 
-    if (stat.isDirectory()) {
-      const subdirRoutes = syncCrawlRoutesDir(path.resolve(dirpath, list[i]), uriPathTokens.slice());
+    const list = fs.readdirSync(dirpath);
+    console.log(dirpath, list);
+    const routes = [];
+    const files = [];
 
-      for (let j = 0; j < subdirRoutes.length; j++) {
-        routes.push(subdirRoutes[j]);
+    sortInsensitive(list);
+
+    for (let i = 0; i < list.length; i++) {
+      const stat = fs.statSync(path.resolve(dirpath, list[i]));
+
+      if (stat.isFile() && jsFileExt.test(list[i])) {
+        files.push(list[i]);
+        continue;
+      }
+
+      if (stat.isDirectory()) {
+        const subdirRoutes = getRoutes(path.resolve(dirpath, list[i]), uriPathTokens.slice());
+
+        for (let j = 0; j < subdirRoutes.length; j++) {
+          routes.push(subdirRoutes[j]);
+        }
       }
     }
-  }
 
-  for (let i = 0; i < files.length; i++) {
-    const verb = files[i].replace(jsFileExt, '').toLowerCase();
+    for (let i = 0; i < files.length; i++) {
+      const verb = files[i].replace(jsFileExt, '').toLowerCase();
 
-    if (!validVerbs.includes(verb)) {
-      continue;
+      if (!validVerbs.includes(verb)) {
+        continue;
+      }
+
+      const individualRoute = require(path.resolve(dirpath, files[i]));
+      routes.push(individualRoute.expressRouter(verb, '/' + uriPathTokens.join('/')));
     }
 
-    const individualRoute = require(path.resolve(dirpath, files[i]));
-    routes.push(individualRoute.expressRouter(verb, '/' + uriPathTokens.join('/')));
+    return routes;
   }
-  
-  return routes;
+
+  return getRoutes(rootpath);
 }
 
 module.exports = syncCrawlRoutesDir;
