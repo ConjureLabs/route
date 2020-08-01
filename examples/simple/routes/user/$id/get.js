500e6058cbfe729af3dc0da0e268152f3501e6f3
Tim Marshall <tim@marshall.io>
2020-08-01 13:52:09 -0700

    simplify this file as well

--- examples/simple/routes/user/$id/get.js
+++ examples/simple/routes/user/$id/get.js
@@ -1,11 +1,5 @@
-const Route = require('@conjurelabs/route')
-
-const route = new Route()
-
-route.push((req, res) => {
+module.exports = ((req, res) => {
   const { id } = req.params
 
   res.send(`User id is ${id}`)
 })
-
-module.exports = route
